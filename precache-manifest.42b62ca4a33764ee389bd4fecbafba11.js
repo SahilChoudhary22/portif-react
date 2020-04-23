cc969cfe7c2ebd9d7df53c53180fc92a5931b30b
Sahil Choudhary <sahil.codes.it@gmail.com>
2020-04-23 23:32:02 +0530

    Updates

---
+++ precache-manifest.42b62ca4a33764ee389bd4fecbafba11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "595bed5d8bae6263b58bd9f61bfcf07b",
+    "url": "/portif-react/index.html"
+  },
+  {
+    "revision": "e7fb012d6dedec9ffed9",
+    "url": "/portif-react/static/css/main.d8842bd7.chunk.css"
+  },
+  {
+    "revision": "529fcc81c392bac7f681",
+    "url": "/portif-react/static/js/2.6712e69f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portif-react/static/js/2.6712e69f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7fb012d6dedec9ffed9",
+    "url": "/portif-react/static/js/main.dfb34cdc.chunk.js"
+  },
+  {
+    "revision": "b8331ab408cf71f8395f",
+    "url": "/portif-react/static/js/runtime-main.99a2426a.js"
+  }
+]);
